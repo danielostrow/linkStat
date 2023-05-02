f47ec3aba31f397937956a956399ae3818caac41
Daniel Ostrow <101360318+danielostrow@users.noreply.github.com>
2023-05-02 00:19:01 -0400

    Create linkStat.py

---
+++ linkStat.py
@@ -0,0 +1,22 @@
+import json
+import requests
+from tabulate import tabulate
+
+# Load JSON data from file
+with open('links.json', 'r') as f:
+    data = json.load(f)
+
+# Need a store for the results
+results = []
+
+# links in JSON data
+for link in data['links']:
+    # Send request and get response code
+    response = requests.get(link)
+    status_code = response.status_code
+    
+    # Add to result store
+    results.append([link, status_code])
+
+# Print store as table
+print(tabulate(results, headers=['Link', 'Status']))
